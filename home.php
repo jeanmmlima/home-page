877b353f2b352919df3144d0b9f302ecd44accb9
jeanmmlima <jeanmariomlima@gmail.com>
2019-03-12 13:52:29 -0300

    icons of contacts

--- home.php
+++ home.php
@@ -11,6 +11,10 @@
   <link rel='shortcut icon' type='image/gif' href='img/DCAicon.gif'>
 
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/  font-awesome/4.7.0/css/font-awesome.min.css">
+  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
   
@@ -144,12 +148,16 @@
         <!-- 
       # CONTATO
     --> 
-    <section id='academic-prod'>
+    <section id='contact'>
         <div class='row'>
             <div class='text'>
              <h3 >Contato</h3>
-             <p>UFRN-CT-DCA</p>
-             <p>email: jean@dca.ufrn.br</p>
+             <span>
+              <i class="fa fa-bank"></i>  UFRN-CT-DCA
+             </span><br>
+             <span>
+              <i class="fa fa-envelope"></i>  jean@dca.ufrn.br
+            </span><br>
              <p>telefone: +55 84 3342-2231 (222)</p>
              <p>github: jeanmmlima</p>
             
