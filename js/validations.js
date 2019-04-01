1fc6af98203bf6fba4a9ece7804e38b0ffa63bad
jeanmmlima <jeanmariomlima@gmail.com>
2019-04-01 16:23:41 -0300

    validators ok

--- js/validations.js
+++ js/validations.js
@@ -10,17 +10,48 @@ function validator(id_grupo, id_horario, id_data) {
   if (grupo == 0) {
     text = "Grupo: Campo obrigatório!"
     document.getElementById("l_grupo").innerHTML = text;
+    document.getElementById("l_grupo").style.color = "red";
+  } else {
+    text = "Grupo:"
+    document.getElementById("l_grupo").innerHTML = text;
+    document.getElementById("l_grupo").style.color = "black";
   } 
 
   if (horario == 0){
     text = "Horário: Campo obrigatório!"
     document.getElementById("l_horario").innerHTML = text;
+    document.getElementById("l_horario").style.color = "red";
+  } else {
+    text = "Horário:"
+    document.getElementById("l_horario").innerHTML = text;
+    document.getElementById("l_horario").style.color = "black";
   }
 
   if (!isNaN(data.valueOf())){
     text = "Data: Campo obrigatório!"
     document.getElementById("l_data").innerHTML = text;
+    document.getElementById("l_data").style.color = "red";
+  } else {
+    text = "Data:"
+    document.getElementById("l_data").innerHTML = text;
+    document.getElementById("l_data").style.color = "black";
   }
+
+
+}
+
+function clearValidators(){
+    text = "Grupo:"
+    document.getElementById("l_grupo").innerHTML = text;
+    document.getElementById("l_grupo").style.color = "black"
+
+    text = "Horário:"
+    document.getElementById("l_horario").innerHTML = text;
+    document.getElementById("l_horario").style.color = "black";
+
+    text = "Data:"
+    document.getElementById("l_data").innerHTML = text;
+    document.getElementById("l_data").style.color = "black";
 
 
 }
